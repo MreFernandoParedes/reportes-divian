9e2192f2c035465af181b3120d1a14f7d43453cc
MreFernandoParedes <fparedesa@rree.gob.pe>
2026-01-15 10:35:08 -0500

    Initialize Express server with static file serving

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname)); // Servir archivos estáticos
+app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
